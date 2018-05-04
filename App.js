76ff4326c545828af9c3c67ed28efcd6035791db
factsfinder <phanindrasrikar@gmail.com>
2018-05-03 22:31:37 -0400

    Revert "added dependencies to fix 'crypto not found error' in react-antive"
    This reverts commit 9e3c08ff1e8ea7a3be98879f6d0faf1170765136.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry ,StyleSheet, Text, View, Settings } from 'react-native';
 import Main from './src/Main';
 
 export default class App extends Component {
@@ -11,6 +11,8 @@ export default class App extends Component {
     );
   }
 }
+
+AppRegistry.registerComponent('App', () => Main);
 
 const styles = StyleSheet.create({
   AppContainer: {
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import './shim.js';
 
 AppRegistry.registerComponent('Zeus', () => App);
